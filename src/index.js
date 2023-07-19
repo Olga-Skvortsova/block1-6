14f2f9968c03622aa0031b33ed99a3dbb5697cae
ольга скворцова <olya21062003@gmail.com>
2023-07-19 19:07:24 +0300

    init: project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,149 @@
 import './index.html';
-import './index.scss';
-import code from './img/code.png'
-import { mult, sum } from './modules/calc';
+import './index.sass';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+import { menu, burger, shadow, navigation, burgerLines, head, swiperReadMore, swiperContainer, swiperWrapper, menuWrapper, lineInNav, callButton, callBlock, callCross, bellButton, bellBlock, bellCross, servicesReadMore, services, servicesTextBlock,  servicesText, servicesSpan, servicesHidden, readMoreButon, servicesSpan2, servicesContainer, swiperContainer2, swiperWrapper2, swiperReadMore2, swiperRepair } from './modules/calc';
 
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append(img);
+burger.addEventListener('click', changeColor);
+menuWrapper.addEventListener('click', changeColor);
+swiperReadMore.addEventListener('click', readMoreAvailable);
+callButton.addEventListener('click', callModuleActive);
+callCross.addEventListener('click', callModuleActive);
+bellButton.addEventListener('click', bellModuleActive);
+bellCross.addEventListener('click', bellModuleActive);
+servicesReadMore.addEventListener('click', readMoreService);
+swiperReadMore2.addEventListener('click', readMoreAvailable2);
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+function readMoreService() {
+    readMoreButon.classList.toggle('displaynone');
+    servicesHidden.classList.toggle('flexdisplay');
+    servicesReadMore.firstElementChild.classList.toggle('imgtransform');
+    if(document.documentElement.clientWidth < 750) {
+        services.classList.toggle('serices-read-more-block');
+        servicesTextBlock.classList.toggle('serices-read-more-text');
+        servicesText.classList.toggle('serices-text');
+        servicesSpan.classList.toggle('flexdisplay');
+    } else if(document.documentElement.clientWidth > 750 && document.documentElement.clientWidth < 1090) {
+        servicesSpan2.classList.toggle('flexdisplay');
+        servicesContainer.classList.toggle('section1-background2');
+        servicesReadMore.classList.toggle('serices-read-more2');
+    }
+}
+
+menuWrapper.addEventListener('click', function(event) {
+    if (event.target.classList.contains('menu-wrapper')) {
+        event.stopPropagation();
+        return;
+    }
+    changeColor();
+});
+
+function changeColor() {
+    menuWrapper.classList.toggle('menu-wrapper-active');
+    menu.classList.toggle('menu_active');
+    burgerLines[0].classList.toggle('head__burger-line-change-1');
+    burgerLines[1].classList.toggle('head__burger-line-change-2');
+    burgerLines[2].classList.toggle('head__burger-line-change-3');
+    navigation.classList.toggle('hidden');
+    shadow.classList.toggle('hidden');
+    head.classList.toggle('absolute-burger');
+    lineInNav.classList.toggle('displaynone');
+}
+
+function callModuleActive(){
+    callBlock.classList.toggle('call-active');
+    head.classList.toggle('displaynone');
+}
+
+function bellModuleActive(){
+    bellBlock.classList.toggle('bell-active');
+    head.classList.toggle('displaynone');
+}
+
+function initServiceSwiper() {
+    const serviceSwiper = new Swiper('.services__swiper-container', {
+        spaceBetween: 19.5,
+        freeMode: true,
+        slidesPerView: 1.6,
+    });
+}
+
+function initSlider() {
+    const swiper= new Swiper('.swiper-container', {
+        modules: [Pagination],
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        slidesPerView: 1.25,
+        spaceBetween: 16,
+        centeredSlides: true,
+    });
+    const priceSwiper = new Swiper('.swiperprice__container', {
+        slidesPerView: 1.2,
+        spaceBetween: 15,
+        centeredSlides: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+}
+
+
+function detectDevice() {
+    if(document.documentElement.clientWidth < 750){
+        initSlider();
+    };
+}
+detectDevice();  
+
+function detectDevice2() {
+    if(document.documentElement.clientWidth < 1103) {
+        initServiceSwiper();
+    }
+}
+detectDevice2();
+
+function readMoreAvailable () {
+    if(document.documentElement.clientWidth > 750 && document.documentElement.clientWidth < 1090){
+        swiperContainer.classList.toggle('swiper-сontainer-height-more');
+        swiperWrapper.children[6].classList.toggle('flexdisplay');
+        swiperWrapper.children[7].classList.toggle('flexdisplay');
+        swiperWrapper.classList.toggle('swiper-wrapper-height-more');
+        swiperReadMore.firstElementChild.classList.toggle('imgtransform');
+        swiperReadMore.lastElementChild.classList.toggle('displaynone');
+        swiperReadMore.children[1].classList.toggle('flexdisplay');
+    }else if(document.documentElement.clientWidth > 1100){
+        swiperContainer.classList.toggle('swiper-сontainer-height-more');
+        swiperWrapper.children[8].classList.toggle('flexdisplay');
+        swiperWrapper.children[9].classList.toggle('flexdisplay');
+        swiperWrapper.children[10].classList.toggle('flexdisplay');
+        swiperWrapper.classList.toggle('swiper-wrapper-height-more');
+        swiperReadMore.firstElementChild.classList.toggle('imgtransform');
+        swiperReadMore.lastElementChild.classList.toggle('displaynone');
+        swiperReadMore.children[1].classList.toggle('flexdisplay');
+    };
+};
+
+function readMoreAvailable2 () {
+    swiperReadMore2.firstElementChild.classList.toggle('imgtransform');
+    swiperReadMore2.lastElementChild.classList.toggle('displaynone');
+    swiperReadMore2.children[1].classList.toggle('flexdisplay');
+    if(document.documentElement.clientWidth > 750 && document.documentElement.clientWidth < 1090){
+        swiperContainer2.classList.toggle('repair-сontainer-height-more')
+        swiperWrapper2.classList.toggle('repair-wrapper-height-more');
+        swiperRepair.classList.toggle('repair-сontainer-height-more');
+        swiperWrapper2.children[3].classList.toggle('margintop22');
+        swiperWrapper2.children[4].classList.toggle('margintop22');
+        swiperWrapper2.children[5].classList.toggle('margintop22');
+    }else if(document.documentElement.clientWidth > 1100){
+        swiperContainer2.classList.toggle('repair-сontainer-height-more')
+        swiperWrapper2.classList.toggle('repair-wrapper-height-more');
+        swiperRepair.classList.toggle('repair-сontainer-height-more');
+        swiperWrapper2.children[4].classList.toggle('margintop22');
+        swiperWrapper2.children[5].classList.toggle('margintop22');
+        swiperWrapper2.children[6].classList.toggle('margintop22');
+        swiperWrapper2.children[7].classList.toggle('margintop22');
+    };
+};
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,2 +1,37 @@
-export const mult = (a, b) => a * b;
-export const sum = async(a, b) => a + b;
+export const menu = document.querySelector(".menu")
+export const menuWrapper = document.querySelector(".menu-wrapper")
+export const burger = document.querySelector(".head__burger")
+export const shadow = document.querySelector(".head__shadow")
+export const navigation = document.querySelector(".head__navigation")
+export const burgerLines = burger.children[0].children
+export const head = document.querySelector(".head")
+export const body = document.querySelector("body")
+export const swiperReadMore = document.querySelector(".swiper__readmore")
+export const swiperContainer = document.querySelector(".swiper__container")
+export const swiperWrapper = document.querySelector(".swiper__wrapper")
+export const lineInNav = document.querySelector(".head__line-in-nav")
+export const services = document.querySelector(".services")
+
+export const swiperRepair = document.querySelector(".swiper-repair")
+export const swiperContainer2 = document.querySelector(".swiper-repair__container")
+export const swiperWrapper2 = document.querySelector(".swiper-repair__wrapper")
+export const swiperReadMore2 = document.querySelector(".repair-swiper-readmore")
+
+
+export const callButton = document.querySelector(".callbuton")
+export const callBlock = document.querySelector(".call")
+export const callCross = document.querySelector(".call__cross")
+
+export const bellButton = document.querySelector(".bellbuton")
+export const bellBlock = document.querySelector(".bell")
+export const bellCross = document.querySelector(".bell__cross")
+
+export const servicesReadMore = document.querySelector(".services__read-more")
+export const servicesTextBlock = document.querySelector(".services__text-and-more") 
+export const servicesText = document.querySelector(".services__text")
+export const servicesSpan = document.querySelector(".services-span")
+export const servicesHidden = document.querySelector(".services__hiden")
+export const readMoreButon = document.querySelector(".services__readmorebuton")
+
+export const servicesSpan2 = document.querySelector(".services-span2")
+export const servicesContainer = document.querySelector(".section1-background")
